e8ef85bf760205367a6d66d4e6168566406116b3
Yoshorein <oscar.grivelet@gmail.com>
2025-04-04 14:19:37 +0200

    first commit

---
+++ lib/constante/constante.h
@@ -0,0 +1,31 @@
+namespace Pin
+{
+    namespace Driver
+    {
+        constexpr int 
+        DIR_G=1,
+        STEP_G=2,
+        DIR_D=3,
+        STEP_D=4,
+        EN=9;
+    } // namespace Driver
+    
+    namespace IHM
+    {
+        constexpr int 
+        TIRETTE=11;
+
+    } // namespace IHM 
+    
+} // namespace Pin
+
+namespace physique
+{
+    constexpr double 
+    LARGEUR=35.6,
+    ECRT_ROUE= 26,
+    DIAM_ROUE=75,
+    STEP_REV=200,
+    STEP_CM=(3.1415926535)
+} // namespace physique
+
